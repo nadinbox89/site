50381a9c1e905b2c1e452e703b7c41ad83c9e826
nadin <nadinbox89@gmail.com>
2017-03-08 01:44:30 +0300

    Update publish URL, fix cmd and sh username/password template

--- build.cmd
+++ build.cmd
@@ -1,2 +1,1 @@
-#!/usr/bin/env bash
 java -Dusername=user -Dpassword=pass -jar mvn-classloader-1.8.jar  com.github.igor-suhorukov:maven-cli:3.3.9 com.github.igorsuhorukov.maven.MavenRunner clean package scm-publish:publish-scm
